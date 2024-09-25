8c9cc541b73432f1abf0ccd2edf7417543d85935
JuanchoGonzalez <juancruzg30@gmail.com>
2024-09-25 11:30:39 -0300

    modify app and csv

--- server/app.py
+++ server/app.py
@@ -6,14 +6,17 @@ import numpy as np
 import pandas as pd
 import os
 from langchain_community.llms import Ollama
-# Ruta al archivo CSV que contiene las etiquetas
+
+# csv que contiene el nombre y las etiquetas
 csv_file = 'dataset_clean.csv'
 df = pd.read_csv(csv_file)
-# Extraer las clases únicas desde el CSV
-class_labels = df['label'].unique()  # Todas las clases únicas desde el CSV
+
+class_labels = df['label'].unique()
+
 app = Flask(__name__)
 cors = CORS(app, origins='*')
-model_path = '/home/juancho_gonzalez/Escritorio/Proyecto/server/CookingWithAI/server/modeloEntrenado'  # si es una carpeta
+model_path = '/home/juancho_gonzalez/Escritorio/Proyecto/server/CookingWithAI/server/modeloEntrenado'
+
 # Asegúrate de que el archivo existe
 if os.path.exists(model_path):
     print(f'La ruta {model_path} es válida y existe.')
@@ -22,40 +25,46 @@ if os.path.exists(model_path):
     print("Modelo cargado correctamente.")
 else:
     print(f'La ruta {model_path} no existe o es incorrecta.')
-# Configurar Ollama
-ollama = Ollama(
-    base_url='http://localhost:11434',
-    model="gemma2:2b"
-)
-# Preprocesar la imagen antes de realizar la predicción
+
+# procesar la imagen antes de hacer la predicción
 def preprocess_image(image):
-    """Preprocesa la imagen de entrada de manera consistente con el entrenamiento"""
-    image = image.resize((224, 224))  # Redimensionar al mismo tamaño usado en el entrenamiento
+
+    image = image.resize((224, 224))  # Redimensionar al mismo tamanio usado en el entrenamiento
     img_array = np.array(image)
     img_array = np.expand_dims(img_array, axis=0)  # Añadir una dimensión extra para el batch
     img_array = img_array / 255.0  # Normalizar como se hizo en el entrenamiento
     return img_array
+
 # Obtener los ingredientes a partir de la imagen utilizando el modelo entrenado
 def get_ingredients_from_image(image):
-    """Realiza la predicción utilizando el modelo cargado"""
     img_array = preprocess_image(image)
-    # Hacer la predicción con el modelo
-    predictions = model.predict(img_array)
+    predictions = model.predict(img_array) # prediccion
+    
     # Obtener la clase con mayor probabilidad
     predicted_class = np.argmax(predictions, axis=1)[0]  # Obtener el índice de la clase predicha
     # Obtener el nombre de la clase predicha utilizando el índice
     ingredient = class_labels[predicted_class]
     return ingredient
+
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template("index.html")
+
+# Conexion Ollama
+ollama = Ollama(
+    base_url='http://localhost:11434',
+    model="gemma2:2b"
+)
+
+# ya con el modelo cargado y predecido ,consultarle a ollama 
 @app.route('/consulta_ollama', methods=['POST'])
 def consulta_ollama():
     try:
         if 'image' in request.files:
             image_file = request.files['image']
             image = Image.open(image_file)
-            # Obtener los ingredientes de la imagen utilizando la función anterior
+
             ingredient = get_ingredients_from_image(image)
             print(f"Predicted ingredient: {ingredient}")
             if not ingredient:
@@ -69,5 +78,7 @@ def consulta_ollama():
             return jsonify({'error': 'No image found in the request'})
     except Exception as e:
         return jsonify({'error': str(e)})
+    
+# ejecucion
 if __name__ == "__main__":
     app.run(debug=True)
